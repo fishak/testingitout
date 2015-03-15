081b838ea1cb30cd26ab9a36fac05e5be202f21a
fishak <faridishak.quota@gmail.com>
2015-03-15 18:12:44 +0100

    added more text

--- acode.sql
+++ acode.sql
@@ -17,3 +17,6 @@ AND 1 = 3
 AND 2 = 2
 well this file is not an sql file anymore but some text file,....
 I am just going to add more text and see how this this works out. 
+
+Well just now I added some text to the other acode.sql file and pushed it. 
+I have not yet git pulled. Just saved the file in the folder. 
