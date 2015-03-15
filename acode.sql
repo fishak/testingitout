30b144f63417b0613a38354716c44de0e62cf357
fishak <faridishak.quota@gmail.com>
2015-03-15 18:14:01 +0100

    adde text

--- acode.sql
+++ acode.sql
@@ -15,3 +15,7 @@ AND 1 = 2
 AND 1 = 4
 AND 1 = 3
 AND 2 = 2
+In the second folder on my machine I am changing the file with other text while not adding 
+or committing and pushing, but I will do some pushing of this file. 
+Just want to know how the file will be handled.
+Just added text to other file and just saved it, I will push the changes of this file right now.
